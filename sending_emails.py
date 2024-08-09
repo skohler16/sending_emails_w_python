249394d11a6be3973c011d902fd340cf42715200
Scooter C Kohler <skohler16@gmail.com>
2024-08-09 14:21:36 -0700

    added some flexability to our email server

--- sending_emails.py
+++ sending_emails.py
@@ -1,17 +1,21 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
-email['from'] = "Scooter"
-email['to'] = 'skohler16@gmail.com'
-email['subject'] = 'This is a test and only a test'
+email['From'] = "Scooter"
+email['To'] = 'skohler16@gmail.com'
+email['Subject'] = 'This is NOT a test and you just won a ton!!'
 
-email.set_content('He is a python master')
+# email.set_content(html.substitute(name='TinTin'))
 
 with smtplib.SMTP_SSL(host='smtp.gmail.com', port=465) as connection:
+    connection.ehlo()
     email_address = 'sdaicmd@gmail.com'
     email_password = 'muvi xmga ubmf pqto'
     connection.login(email_address, email_password )
     connection.sendmail(from_addr=email_address, to_addrs='skohler16@gmail.com',
-    msg="subject:hi \n\n this is my message")
+    msg = html.substitute(name = 'TinTin'))
     print('all good boss!')
